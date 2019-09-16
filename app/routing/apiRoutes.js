53d75ad0c02e46667aabbfb0c71752e7e6749b76
Nina Shin <48607661+ninashin66@users.noreply.github.com>
2019-09-15 22:54:55 -0600

    updated routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,22 +4,4 @@ module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
-
-  app.post("/api/friends", function(req, res) {
-    // if (tableData.length < 5) {
-    //   tableData.push(req.body);
-    //   res.json(true);
-    // }
-    // else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
-  });
-
-  app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-    friendsData.length = 0;
-
-    res.json({ ok: true });
-  });
 };
